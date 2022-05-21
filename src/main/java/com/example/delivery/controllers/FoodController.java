4fba3940edd093e851a7e1f4a7a914f58bdb56be
Kittipong Theothaisong <kittipong.knos@gmail.com>
2022-05-21 22:56:32 +0700

    added api for delete food by id

--- src/main/java/com/example/delivery/controllers/FoodController.java
+++ src/main/java/com/example/delivery/controllers/FoodController.java
@@ -1,12 +1,10 @@
 package com.example.delivery.controllers;
 
+import com.example.delivery.ConstantMessages;
 import com.example.delivery.models.Food;
 import com.example.delivery.responses.OkResponse;
 import com.example.delivery.services.FoodService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/foods")
@@ -21,5 +19,11 @@ public class FoodController {
     @GetMapping("/{id}")
     public OkResponse<Food> getFoodById(@PathVariable Long id) {
         return OkResponse.of(foodService.getFoodById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public OkResponse<String> deleteFoodById(@PathVariable Long id) {
+        foodService.deleteFoodById(id);
+        return OkResponse.of(ConstantMessages.SUCCESS);
     }
 }

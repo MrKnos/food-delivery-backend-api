13de3d00285e9574cb6f5e1f5e413fbcf97e3761
Kittipong Theothaisong <kittipong.knos@gmail.com>
2022-05-22 22:34:00 +0700

    added api for add food options

--- src/main/java/com/example/delivery/services/FoodService.java
+++ src/main/java/com/example/delivery/services/FoodService.java
@@ -1,18 +1,30 @@
 package com.example.delivery.services;
 
+import com.example.delivery.entities.FoodEntity;
+import com.example.delivery.entities.FoodOptionEntity;
 import com.example.delivery.exceptions.FoodNotFoundException;
 import com.example.delivery.models.Food;
+import com.example.delivery.models.FoodOption;
+import com.example.delivery.reopositories.FoodOptionRepository;
 import com.example.delivery.reopositories.FoodRepository;
+import com.google.common.collect.ImmutableList;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FoodService {
 
-    public FoodService(FoodRepository foodRepository) {
+    public FoodService(
+            FoodRepository foodRepository,
+            FoodOptionRepository optionRepository
+    ) {
         this.foodRepository = foodRepository;
+        this.optionRepository = optionRepository;
     }
 
-    public FoodRepository foodRepository;
+    FoodRepository foodRepository;
+    FoodOptionRepository optionRepository;
 
     public Food getFoodById(Long id) {
         return foodRepository.findById(id)
@@ -26,5 +38,21 @@ public class FoodService {
         }
 
         foodRepository.deleteById(id);
+    }
+
+    public void addOptions(
+            Long foodId,
+            ImmutableList<FoodOption> options
+    ) {
+        final FoodEntity food = foodRepository
+                .findById(foodId)
+                .orElseThrow(() -> new FoodNotFoundException(foodId));
+
+        final List<FoodOptionEntity> optionEntities = options
+                .stream().map(FoodOptionEntity::fromModel)
+                .toList();
+
+        optionEntities.forEach(option -> option.setFood(food));
+        optionRepository.saveAll(optionEntities);
     }
 }
